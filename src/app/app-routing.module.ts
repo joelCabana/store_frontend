9c9f6098cb1a32056cde1bf23a80b38212266e9d
joelCabana <joelxcabana@gmail.com>
2021-02-08 22:27:20 -0300

    Navigation - Settings

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,25 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path:'home',loadChildren: () => 
+      import('./@public/pages/home/home.module').then(m => m.HomeModule) 
+  },
+  { path:'contact',loadChildren: () => 
+      import('./@public/pages/contact/contact.module').then(m => m.ContactModule) 
+  },
+  { path: ``, redirectTo: `home`, pathMatch: `full` },
+  { path: '**', redirectTo: `home`, pathMatch: `full` },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,
+    {
+      useHash:true,
+      scrollPositionRestoration:'enabled'
+    }
+  )],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
