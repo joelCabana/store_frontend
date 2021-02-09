c4f7260055b7e6b678adecf646432e6c5cd98d73
joelCabana <joelxcabana@gmail.com>
2021-02-08 22:43:05 -0300

    Panel Admin - Creacion de paginas y configuracion de las rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,21 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
 
-  { path:'home',loadChildren: () => 
+  { 
+    path:'home',loadChildren: () => 
       import('./@public/pages/home/home.module').then(m => m.HomeModule) 
   },
-  { path:'contact',loadChildren: () => 
+  { 
+    path:'contact',loadChildren: () => 
       import('./@public/pages/contact/contact.module').then(m => m.ContactModule) 
+  },
+  { 
+    path:'admin',loadChildren: () => 
+      import('./@admin/pages/dashboard/dashboard.module').then(m => m.DashboardModule) 
+  },
+  { 
+    path:'admin/users',loadChildren: () => 
+      import('./@admin/pages/users/users.module').then(m => m.UsersModule) 
   },
   { path: ``, redirectTo: `home`, pathMatch: `full` },
   { path: '**', redirectTo: `home`, pathMatch: `full` },
